0c84f8cf9ec2954c34a8afd8e9cecdfcfa3a38b8
Redouan Bouziza <redouan.bouziza@hva.nl>
2023-06-02 21:58:48 +0200

    Added a pic and changed theme and color

--- app/src/main/java/com/example/capstoneredouan/ui/theme/Color.kt
+++ app/src/main/java/com/example/capstoneredouan/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.example.capstoneredouan.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val GreenAppLight = Color(0xFF98B899)
+val GreenApp = Color(0xFF0D9810)
+val GreenAppDark = Color(0xFF047806)
--- app/src/main/java/com/example/capstoneredouan/ui/theme/Theme.kt
+++ app/src/main/java/com/example/capstoneredouan/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = GreenAppLight,
+    primaryVariant = GreenAppDark,
+    secondary = GreenApp
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = GreenApp,
+    primaryVariant = GreenAppDark,
+    secondary = GreenApp
 
     /* Other default colors to override
     background = Color.White,
