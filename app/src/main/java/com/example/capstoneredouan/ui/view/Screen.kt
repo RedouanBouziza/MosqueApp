8a12042c521f0d19ccf321884872d5ee07286eae
Redouan Bouziza <redouan.bouziza@hva.nl>
2023-06-04 16:43:46 +0200

    Added icons for each specific

--- app/src/main/java/com/example/capstoneredouan/ui/view/Screen.kt
+++ app/src/main/java/com/example/capstoneredouan/ui/view/Screen.kt
@@ -1,12 +1,29 @@
 package com.example.capstoneredouan.ui.view
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Alarm
+import androidx.compose.material.icons.filled.AppRegistration
+import androidx.compose.material.icons.filled.Bedtime
+import androidx.compose.material.icons.filled.Explore
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.HowToReg
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Login
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Mosque
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material.icons.filled.WatchLater
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.capstoneredouan.R
+
 sealed class Screen(
-    val route: String
+    val route: String,
+    val title: Int,
+    val icon: ImageVector
 ) {
-    object HomeScreen: Screen("home_screen")
-    object PrayerTimesScreen: Screen("prayer_times_screen")
-    object MapScreen: Screen("map_screen")
-    object CompassScreen: Screen("compass_screen")
-    object Login: Screen("login")
-    object Registration: Screen("registration")
+    object PrayerTimesScreen: Screen("prayer_times_screen", R.string.prayer_times, Icons.Default.WatchLater)
+    object MapScreen: Screen("map_screen", R.string.map, Icons.Default.LocationOn)
+    object CompassScreen: Screen("compass_screen", R.string.compass, Icons.Default.Explore)
+    object Login: Screen("login", R.string.login, Icons.Default.Login)
+    object Registration: Screen("registration", R.string.log_out, Icons.Default.HowToReg)
 }
